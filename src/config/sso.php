5cffc06c831b1494fcb9b7e54955cb8b62e796a4
牟勇 <my24251325@gmail.com>
2018-08-15 03:44:23 +0000

    删除回调

--- src/config/sso.php
+++ src/config/sso.php
@@ -15,14 +15,4 @@ return [
      * sso ticket cache
      */
     'cache_prefix' => 'laravel.sso.',
-
-    /**
-     * if your want to login in as a specific user, your can modify as you need. Or you can just return $sso.
-     */
-    'get_user_by_sso' => function ($sso) {
-
-        //return User::find($sso['user_id']);
-
-        return $sso;
-    },
 ];
